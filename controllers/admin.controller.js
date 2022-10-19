da6d89f5e80d3debc89f643640623560414119c9
Uzair Vawda <uzair.vawda@gmail.com>
2022-10-19 19:09:58 -0400

    adding multer as middleware and allowing for product submission without DB storage

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -6,10 +6,19 @@ function getNewProduct(req, res) {
 	res.render('admin/products/newProduct')
 }
 
-function createNewProduct() {}
+function getAllOrders(req, res) {
+	res.render('admin/products/allOrders')
+}
+
+function createNewProduct(req, res) {
+	console.log(req.body);
+	console.log(req.file);
+	res.redirect('/admin/products')
+}
 
 module.exports = {
 	getProducts: getProducts,
 	getNewProduct: getNewProduct,
+	getAllOrders: getAllOrders, 
 	createNewProduct: createNewProduct
 }
