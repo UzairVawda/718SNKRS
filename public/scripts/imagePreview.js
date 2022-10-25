473400ec732fc534071b50486661effd6cc5bbb1
Uzair Vawda <uzair.vawda@gmail.com>
2022-10-25 15:56:58 -0400

    image preview

---
+++ public/scripts/imagePreview.js
@@ -0,0 +1,18 @@
+const imagePicker = document.getElementById("productImage")
+const imagePreview = document.getElementById("productImagePreview")
+
+function updateImagePreview() {
+	const files = imagePicker.files;
+	if(!files || files.length === 0) {
+		imagePreview.style.display = 'none';
+		return
+	}
+	const pickedFile = files[0];
+	console.log(pickedFile)
+	const srcURL = URL.createObjectURL(pickedFile);
+	console.log(srcURL)
+	imagePreview.src = srcURL
+	imagePreview.style.display = 'block';
+}
+
+imagePicker.addEventListener('click', updateImagePreview)
