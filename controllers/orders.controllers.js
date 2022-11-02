eb45956068592749afd54a5ff361e4c38fb68854
Uzair Vawda <uzair.vawda@gmail.com>
2022-11-02 19:28:00 -0400

    adding stripe api

--- controllers/orders.controllers.js
+++ controllers/orders.controllers.js
@@ -1,5 +1,6 @@
 const Order = require("../models/order.model");
 const User = require("../models/User.model");
+const stripe = require('stripe')('sk_test_51LzpfMBVSj0YVfxBbjeG3Tti4M2PMTolG8E9XgreklGgXb2jPBXClBZaXcrlkMi97dne1uhgnoip7581BD0K542600etU2V4Gy')
 
 async function getOrders(req, res) {
   try {
@@ -14,6 +15,7 @@ async function getOrders(req, res) {
 
 async function addOrder(req, res, next) {
   const cart = res.locals.cart;
+  console.log(cart.items)
   let userDoc;
   try {
     userDoc = await User.findById(res.locals.uid);
@@ -30,10 +32,39 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect("/orders");
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: cart.items.map(function(item) {
+      return  {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: item.product.title
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100
+        },
+        quantity: item.quantity,
+      }
+    }),
+    mode: 'payment',
+    success_url: 'http://localhost:7180/orders/success',
+    cancel_url: 'http://localhost:7180/orders/failure',
+  });
+
+  res.redirect(303, session.url);
+}
+
+async function getSuccess(req, res) {
+  res.render("customer/orders/success");
+}
+
+async function getFailure(req, res) {
+  res.render("customer/orders/failure");
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure
 };
